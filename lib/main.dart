a27a0d1de20912f10948934ccd731707f1df4c43
Mehedi <mehediehasan@gmail.com>
2024-08-24 15:05:43 +0400

    - First Committ

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        'Casio Watch',
+                        'Casio Watch new',
                         style:
                             TextStyle(fontSize: 30, fontWeight: FontWeight.w800),
                       ),
